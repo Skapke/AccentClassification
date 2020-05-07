dd834c6a087776399f1493103b9d66d16fef7281
ritten <ritten98@hotmail.com>
2020-05-08 00:02:45 +0200

    updated create_csv.py. It now creates three seperate csv files, one for each native speaker category

--- img/create_csv.py
+++ img/create_csv.py
@@ -1,19 +1,36 @@
 import re
 import os
 import csv
+import sys
 
-def get_label(argument):
-    switcher = {
-        "english": 0,
-        "mandarin": 1,
-        "korean": 2
-    }
-    return switcher.get(argument, 0)
+dirs = ["original", "modified"]
 
-with open('subset.csv', 'a', newline='') as file:
-    writer = csv.writer(file)
-    writer.writerow(["file_name", "native_language"])
-    for filename in os.listdir(os.getcwd()+"/original"):
-        r = re.compile("([e-m])\D+")
-        m = r.search(filename)
-        writer.writerow([filename, m.group(0)])
+try:
+    with open('english_subset.csv', 'a', newline='') as english_csv, \
+            open('mandarin_subset.csv', 'a', newline='') as mandarin_csv, \
+            open('korean_subset.csv', 'a', newline='') as korean_csv:
+
+        english_writer = csv.writer(english_csv)
+        english_writer.writerow(["file_name", "native_language"])
+
+        mandarin_writer = csv.writer(mandarin_csv)
+        mandarin_writer.writerow(["file_name", "native_language"])
+
+        korean_writer = csv.writer(korean_csv)
+        korean_writer.writerow(["file_name", "native_language"])
+
+        writer_dict = {"english": english_writer, "mandarin": mandarin_writer, "korean": korean_writer}
+
+        for dir in dirs:
+            path = os.getcwd()+"/"+dir
+            for filename in os.listdir(path):
+                r = re.compile("([e-m])\D+")
+                m = r.search(filename)
+                writer_dict[m.group(0)].writerow([filename, m.group(0)])
+except IOError as e:
+    errno, strerror = e.args
+    print("I/O error({0}): {1}".format(errno, strerror))
+except Exception as other_exception:
+    errno, strerror = other_exception.args
+    print("Unexpected error:".format(errno, strerror), sys.exc_info()[0])
+    raise
